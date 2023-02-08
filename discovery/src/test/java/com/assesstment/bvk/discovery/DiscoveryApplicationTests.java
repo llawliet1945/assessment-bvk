61be76249f1de775e3b48d6de4efc0de25f5fcfb
llawliet <llawliet1945@gmail.com>
2023-02-08 15:55:20 +0700

    initial commit

---
+++ discovery/src/test/java/com/assesstment/bvk/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assesstment.bvk.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
